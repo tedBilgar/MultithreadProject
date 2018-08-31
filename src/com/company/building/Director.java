3a0aad7d3962a90a73596eaf2ba9104bd5944bac
Denis <papa149@mail.ru>
2018-08-31 15:42:22 +0500

    builder(1.5)

--- src/com/company/building/Director.java
+++ src/com/company/building/Director.java
@@ -3,14 +3,17 @@ package com.company.building;
 import com.company.Model.House;
 /*
     Здесь мы можем (необязателен этот класс вообще для паттерна)
-    добавить порядок строительства объекта или осбенностей при ег остроительстве
+    добавить порядок строительства объекта или осбенностей при его строительстве
  */
 public class Director {
     public void constructOwner(Builder builder,House house){
         builder.setHouse(house);
+        builder.setItems();
     }
 
     public void constructThief(Builder builder,House house){
         builder.setHouse(house);
+        builder.setItems();
+        builder.setExtra();
     }
 }
